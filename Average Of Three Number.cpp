a7284c729fbe4b06e84092066efddfe668c8667d
qahirali0705 <qahirali0705@gmail.com>
2026-07-04 15:46:42 +0500

    Add files via upload

---
+++ Average Of Three Number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int num1,num2,num3;
+	cout << "Enter Three Number "<<endl;
+	cin >>num1>>num2>>num3;
+	
+	float avg=(num1+num2+num3)/3;
+	
+	cout<<"Average Number is : "<<avg<<endl;
+	
+	return 0;
+	
+}
